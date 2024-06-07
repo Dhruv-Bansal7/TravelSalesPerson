0471804758095bb8af2bd08c90ddaebfa7fe2207
dhruv-bansal7 <dhruvbansal7727@gmail.com>
2024-06-07 19:49:05 +0530

    Updateion

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const gridSize = 20; // Adjust grid size as needed
+const gridSize = 20; // Grid Size if change it here also change in css file
+
+
+// Cites and there coordinates
 const cities = [
     { id: 0, x: 1, y: 1 },
     { id: 1, x: 5, y: 5 },
@@ -20,76 +23,99 @@ const cities = [
     { id: 14, x: 17, y: 2 },
   ];
 
-function App() {
-  const [tour, setTour] = useState([0]);
-  const [visited, setVisited] = useState([true, false, false, false, false]);
-  const [currentCity, setCurrentCity] = useState(0);
-  const [isAnimating, setIsAnimating] = useState(false);
+//main Function
 
-  useEffect(() => {
-    let interval;
-    if (isAnimating) {
+function App () {
+
+
+  const[tour,setTour] = useState([0]); // Tour array the store the tour of cities
+  const [visited,setVisited] = useState([true]); // visited array that store the bool value that city is visited or not
+  const[currentCity,setCurrentCity] = useState(0); // store current city
+  const [isAnimating,setIsAnimating] = useState(false); // for button
+
+  useEffect(()=> { // if anythig is change it will restart the code automatic
+
+    let interval; // animation interval
+
+    if(isAnimating) {
+
       interval = setInterval(() => {
-        if (tour.length < cities.length) {
-          const nextCity = findNearestNeighbor(currentCity, visited, cities);
-          if (nextCity !== -1) {
+
+        if(tour.length < cities.length) {
+          
+          const nextCity =findNearestNeighour(currentCity,visited,cities); // find nearest neighour of current city
+
+          if(nextCity !== -1){ // if next city is find then set visited to true
+
             setVisited((prev) => {
               const newVisited = [...prev];
               newVisited[nextCity] = true;
               return newVisited;
             });
-            setTour((prev) => [...prev, nextCity]);
-            setCurrentCity(nextCity);
+
+            setTour((prev) => [...prev,nextCity]); // store the next city in tour 
+
+            setCurrentCity(nextCity); // set next city to current city
+
           }
-        } else if (tour.length === cities.length) {
-          setTour((prev) => [...prev, tour[0]]);
-          setIsAnimating(false);
-          clearInterval(interval);
+
+        }
+        else if(tour.length === cities.length) { 
+          setTour((prev) => [...prev,tour[0]]); // if both tour and cities array are equal then return it to the native city by adding 0 in the tour
+
+          setIsAnimating(false); // set animation is set to false 
+
+          clearInterval(interval); // and clear interval
         }
       }, 1000);
     }
     return () => clearInterval(interval);
-  }, [currentCity, tour, visited, isAnimating]);
+  
+  }, [currentCity , tour, visited, isAnimating] );
 
-  const findNearestNeighbor = (currentCity, visited, cities) => {
+  const findNearestNeighour = (currentCity,visited,cities) => { // this function find the nearest Neighour of the current city
     let minDistance = Infinity;
     let nearestCity = -1;
 
-    for (let i = 0; i < cities.length; i++) {
-      if (!visited[i]) {
-        const distance = calculateDistance(cities[currentCity], cities[i]);
-        if (distance < minDistance) {
-          minDistance = distance;
+    for(let i = 0 ; i < cities.length; i++){ // traverse the whole city and find the city which have minimum distance to the current city
+      if(!visited[i]){
+        const distance = calculateDistance(cities[currentCity],cities[i]);
+        if(distance < minDistance){
+          minDistance = distance; // set mindistance to current distance because distance is less then mindistance
           nearestCity = i;
         }
       }
     }
-
-    return nearestCity;
+    return nearestCity; // return the city which have minimum distance
   };
 
-  const calculateDistance = (city1, city2) => {
-    return Math.sqrt(Math.pow(city2.x - city1.x, 2) + Math.pow(city2.y - city1.y, 2));
+  const calculateDistance = (city1,city2) => {
+    return Math.sqrt(Math.pow(city2.x - city1.x, 2) + Math.pow(city2.y - city1.y , 2)); // retur the ditancebetween teo points distance = underroot(x^2 + y^2);
   };
 
-  const startAnimation = () => {
+  const startAnimation = () => { // when button isclixk set default condition
     setTour([0]);
-    setVisited([true, false, false, false, false]);
+    setVisited([true]);
     setCurrentCity(0);
     setIsAnimating(true);
   };
 
   return (
-    <div className="App">
-      <h1>Traveling Salesperson Problem - Nearest Neighbor</h1>
-      <button onClick={startAnimation} disabled={isAnimating}>Start Animation</button>
-      <div className="grid">
-        {Array.from({ length: gridSize }).map((_, row) => (
+    <div className='App'>
+
+      <h1>Travelling SalesPerson Problem - Nearest Neighour</h1>
+
+      <button onClick={startAnimation} disabled= {isAnimating}>Start Animation</button>
+
+      <div className='grid'>
+         {/* Creation of grid */}
+        {Array.from({ length : gridSize }).map((_,row) => (
           <div key={row} className="row">
-            {Array.from({ length: gridSize }).map((_, col) => {
+            {Array.from({ length : gridSize }).map((_,col) => {
               const city = cities.find(city => city.x === col && city.y === row);
               const isVisited = city && visited[city.id];
               return (
+
                 <div
                   key={col}
                   className={`cell ${city ? 'city' : ''} ${isVisited ? 'visited' : ''}`}
@@ -103,19 +129,21 @@ function App() {
             })}
           </div>
         ))}
-        <svg className="path" width="100%" height="100%">
-          {tour.map((cityId, index) => {
-            if (index < tour.length - 1) {
+
+        {/* Creation of path  */}
+        <svg className="path" width= "100%" height="100%">
+          {tour.map((cityId,index) => {
+            if(index < tour.length - 1){
               const nextCityId = tour[index + 1];
               return (
                 <line
                   key={index}
-                  x1={cities[cityId].x * 21 + 10}
-                  y1={cities[cityId].y * 21 + 10}
-                  x2={cities[nextCityId].x * 21 + 10}
-                  y2={cities[nextCityId].y * 21 + 10}
-                  stroke="blue"
-                  strokeWidth="2"
+                  x1 = {cities[cityId].x * 22 + 10}
+                  y1 = {cities[cityId].y * 22 + 10}
+                  x2 = {cities[nextCityId].x * 22 + 10}
+                  y2 = {cities[nextCityId].y * 22 + 10}
+                  stroke='blue'
+                  strokeWidth='2'
                 />
               );
             }
@@ -124,7 +152,9 @@ function App() {
         </svg>
       </div>
     </div>
-  );
+  )
+
+  
 }
 
 export default App;
